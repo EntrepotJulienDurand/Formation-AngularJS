13e8a83b2721f1de72bfa89acbb669bbbb63fb6f
Julien Durand <julien.durand01-ext@pole-emploi.fr>
2015-04-16 15:06:33 +0200

    Refacto

--- controllers/element.js
+++ controllers/element.js
@@ -5,16 +5,9 @@ app
  * Créer un élément
  */
     .controller('creerElement', ['$scope', '$location', function ($scope, $location) {
-        $scope.element = {
-            auteur: null,
-            titre: null,
-            creeLe: null
-        };
 
         $scope.ajouter = function (element) {
             elements.push(element);
-            //détachement du bind
-            $scope.element = {};
             $location.path('elements');
         };
     }])
